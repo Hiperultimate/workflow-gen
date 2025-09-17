93a8bb7260c8c421de6060c9869998e2f36789a4
hiper <hiperultimatevegito@gmail.com>
2025-09-17 21:58:17 +0530

    Fixing webhookRoute to accept path instead of id and wrote logic to get connected nodes

--- apps/server/src/routers/webhookRoutes.ts
+++ apps/server/src/routers/webhookRoutes.ts
@@ -1,28 +1,59 @@
+import type { IConnection, INode } from "@/types/types";
 import { prisma, type Methods } from "@workflow-gen/db";
 import { Router } from "express";
 
-
 const webhookRoutes = Router();
 
-webhookRoutes.all("/:id", async (req, res) => {
-  const { id : webhookId } = req.params;
+webhookRoutes.all("/:path", async (req, res) => {
+  const { path: webhookPath } = req.params;
   const method = req.method as Methods;
 
   // Log the request method and ID
-  console.log(`Received ${method} request for /webhook/${webhookId}`);
+  console.log(`Received ${method} request for /webhook/${webhookPath}`);
 
-  const webhookRecord = await prisma.webhook.findUnique({ where: { id: webhookId , method : method}, include: {workflow : true} });
-
+  const webhookRecord = await prisma.webhook.findFirst({
+    where: { path: webhookPath, method: method },
+    include: { workflow: true },
+  });
+    
   if (!webhookRecord) {
-    return res.status(400).send({ message: "Invalid webhook id. Workflow not found." });
+    return res
+      .status(400)
+      .send({ message: "Invalid webhook path. Workflow not found." });
   }
 
   const workflowData = webhookRecord.workflow;
 
-  console.log("Checking out workflow data : ", workflowData);
+  const webhookId = webhookRecord.id;
+  const nodes = workflowData.nodes as INode[];
+  const connections = workflowData.connections as IConnection[];
 
+  const connectedNodes = getConnectedNodesFromNode(
+    webhookId,
+    nodes,
+    connections
+  );
+
+  console.log("Nodes connected to webhook : ", connectedNodes);
 
   res.status(200).send({ message: "Recieved webhook request, initiating..." });
-})
+});
+
+function getConnectedNodesFromNode(
+  nodeId: string,
+  nodes: INode[],
+  connections: IConnection[]
+) {
+    const nodeMap = new Map(nodes.map((node) => [node.id, node]));
+
+    const connectedNodes = connections
+      .filter((connection) => connection.source === nodeId)
+      .map((connection) => nodeMap.get(connection.target))
+        .filter((node): node is INode => node !== undefined);
+
+      return connectedNodes;
+}
+
+// function processNode(idOfNodeToProcess, inputData) {}
 
 export default webhookRoutes;
---
+++ apps/server/src/types/types.ts
@@ -0,0 +1,27 @@
+export type IConnection = {
+  id: string;
+  type: string;
+  source: string;
+  target: string;
+};
+
+export type INode = {
+  id: string;
+  data: Record<any, any>[];
+  type: NodeType;
+  measured: Record<any, any>[];
+  position: Record<any, any>[];
+  selected: boolean;
+};
+
+export enum NodeTypes {
+  Telegram = "telegramNode",
+  Email = "emailNode",
+  Webhook = "webhookNode",
+  AiAgent = "aiAgent",
+  GeminiModel = "geminiModelNode",
+  CodeTool = "codeToolNode",
+  ManualTriggerNode = "manualTriggerNode",
+}
+
+export type NodeType = `${NodeTypes}`;
